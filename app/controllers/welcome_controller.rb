b4797966682544a5ad6e38b06f83657be8733430
ian brennan <ian.brennan@icloud.com>
2015-01-23 22:37:35 -0700

    late night just saving

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @randomsong = Welcome.get_random_song
   end
 
 end
--- app/models/welcome.rb
+++ app/models/welcome.rb
@@ -1,2 +1,6 @@
 class Welcome
+  def self.get_random_song
+    [1,2,3,4,5,6].sample
+  end
+
 end
